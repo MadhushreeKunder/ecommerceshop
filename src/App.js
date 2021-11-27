fcf15ca3b88b3948c0c656cbffb6dab292f056f0
MadhushreeKunder <madhushreekunder@gmail.com>
2021-11-27 12:58:37 +0530

    make cards and display on productlisting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,105 @@
 import "./styles.css";
 
+
+const products = [
+  {
+    id: 1,
+    name: "Peplum Top",
+    price: 1000, 
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/13719116/2021/3/3/90d0980a-8b39-48bd-9bf9-a67beb14a9741614753528611-Athena-Fuchsia-Pink-top-with-power-shoulder-sleeve-detail-22-1.jpg"
+  },
+  {
+    id: 2,
+    name: "White Mock-Neck",
+    price: 500, 
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/12221944/2020/8/14/b9fc8ee9-ec92-4557-ab60-000e3779291e1597404329638-SASSAFRAS-Women-White-Solid-Top-8171597404327851-1.jpg"
+  },
+  {
+    id: 3,
+    name: "Black Tank Top",
+    price: 100,
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/15102792/2021/9/14/e24aa04a-ce78-42cd-bb7b-e1e8f3d72fad1631604385346-MANGO-Black-Ribbed-Twisted-Styled-Back-Top-7331631604384908-1.jpg"
+  },
+  {
+    id: 4,
+    name: "Cute Strap Top",
+    price: 1121,
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/14340852/2021/8/12/1924ccdd-6bb0-4382-bb8d-77346f65a5ff1628779927372DressesMANGOWomenDressesMANGOWomenTshirtsMANGOWomenTrousersM1.jpg"
+  },
+  {
+    id: 5,
+    name: "Acrylic Jumper",
+    price: 799,
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/15809062/2021/10/13/4f6d1445-f481-4d06-b0b9-6d6de7da9d261634063934609Jumper1.jpg"
+  },
+  {
+    id: 6,
+    name: "Striped Kurta",
+    price: 2990,
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/14485338/2021/8/6/320791a8-feec-4cbb-966b-be3768b339cc1628219628495-Fabindia-Women-Green-Striped-Kurta-7041628219627900-1.jpg"  
+  },
+  {
+    id: 7,
+    name: "Ethnic Kurta Set",
+    price: 1175,
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/10356511/2019/8/8/a28f9ccb-c0d7-4e66-87f0-e639f157ff2d1565263388836-Libas-Women-Kurta-Sets-571565263387250-1.jpg"  },
+  {
+    id: 8,
+    name: "Saree With Cape",
+    price: 1499,
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/13919148/2021/7/9/3ba56466-68a8-44b0-b044-374d9cb0346c1625803181837-metallic-pink-satin-saree-with-foil-jacket-1521625803181354-1.jpg"  },
+  {
+    id: 9,
+    name: "DrawString Playsuit",
+    price: 649,
+img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2021/5/31/16f14729-51d9-4a1b-bb79-68af366ce5491622437948687-1.jpg"  },
+  {
+    id: 10,
+    name: "Flared Mini SKirt",
+    price: 791,
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2019/12/3/fe361135-8cb8-4e6c-a788-ee089b4a0b031575313536330-1.jpg"
+  },
+];
+
+export function ProductListing(){
+
+  return (
+    <div className="container">
+      <h1>Products</h1>
+      <div className="cards-section">
+        {products.map((item)=> (
+          <div className="card">
+            <img className="card-img" src={item.img} alt="" />
+            <div className="card-info">
+              <div>
+                <p className="card-title">{item.name}</p>
+                <small className="card-price">Rs.{item.price}</small>
+              </div>
+              <button className="button button-primary card-add-to-cart">Add to Cart</button>
+            </div>
+            
+          </div>
+
+          
+        ))}
+      </div>
+    </div>
+  )
+
+
+}
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Ecomm</h1>
+      <h1 className="app-header"> Ecomm </h1>
+      <div className="app-body">
+        <button>Add</button>
+      </div>
+      <ProductListing/>
+
+      
     </div>
   );
 }
+
