c23ab3e68d1b9f0e475ae67e9a76bdf3b7053e06
MadhushreeKunder <madhushreekunder@gmail.com>
2021-11-27 21:03:29 +0530

    update cart item quantity

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,7 +1,33 @@
 import { useCart } from "../contexts";
 
 export function Cart() {
-  const { itemsInCart } = useCart();
+  const { itemsInCart, setItemsInCart } = useCart();
+
+  const incrementQuantity = (itemId) => {
+    setItemsInCart((products) =>
+      products.map((product) =>
+        product.id === itemId
+          ? { ...product, quantity: product.quantity + 1 }
+          : product
+      )
+    );
+  };
+
+  const removeItemFromCart = (removeItem) => {
+    setItemsInCart(itemsInCart.filter((product) => product !== removeItem));
+  };
+
+  const decrementQuantity = (itemId) => {
+    setItemsInCart((products) =>
+      products.map((product) =>
+        product.quantity <= 1
+          ? removeItemFromCart(product)
+          : itemId === product.id
+          ? { ...product, quantity: product.quantity - 1 }
+          : product
+      )
+    );
+  };
 
   return (
     <div className="container">
@@ -10,13 +36,26 @@ export function Cart() {
         {itemsInCart.map((item) => (
           <div className="card">
             <img className="card-img" src={item.img} alt="" />
+
             <div className="card-info">
               <p className="card-title">{item.name}</p>
               <small className="card-price">Rs.{item.price}</small>
+
               <div className="cart-update-quantity">
-                <button className="button button-incdec">-</button>
+                <button
+                  className="button button-incdec"
+                  onClick={() => decrementQuantity(item.id)}
+                >
+                  -
+                </button>
                 <small className="cart-quantity">{item.quantity}</small>
-                <button className="button button-incdec">+</button>
+
+                <button
+                  className="button button-incdec"
+                  onClick={() => incrementQuantity(item.id)}
+                >
+                  +
+                </button>
               </div>
             </div>
           </div>
