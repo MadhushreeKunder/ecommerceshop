4564c63f98146b04716e248f1ee280f1e974a83e
MadhushreeKunder <madhushreekunder@gmail.com>
2021-11-27 20:22:29 +0530

    prettify

--- src/App.js
+++ src/App.js
@@ -2,25 +2,29 @@ import { useState } from "react";
 import "./styles.css";
 import { ProductListing, Cart, CartHeader } from "./components";
 
-
-
 export default function App() {
   const [route, setRoute] = useState("products");
   return (
     <div className="App">
       <h1 className="app-header"> Coral-Shop</h1>
       <div className="app-body">
-        <CartHeader/>
-        <button className={route === "products" ? "button-primary button" : "button"} onClick={()=> setRoute("products")}>Products</button>
-        <button className={route === "cart" ? "button-primary button" : "button"} onClick={()=> setRoute("cart")}>Cart</button>
+        <CartHeader />
+        <button
+          className={route === "products" ? "button-primary button" : "button"}
+          onClick={() => setRoute("products")}
+        >
+          Products
+        </button>
+        <button
+          className={route === "cart" ? "button-primary button" : "button"}
+          onClick={() => setRoute("cart")}
+        >
+          Cart
+        </button>
 
-        {route === "products" && <ProductListing/>}
-        {route === "cart" && <Cart/>}
+        {route === "products" && <ProductListing />}
+        {route === "cart" && <Cart />}
       </div>
-      
-
-      
     </div>
   );
 }
-
