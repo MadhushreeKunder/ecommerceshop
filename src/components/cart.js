37fe16d1fc84095fef7e313c6d16183bb3c51a57
MadhushreeKunder <madhushreekunder@gmail.com>
2021-12-18 01:42:19 +0530

    add totalcost

--- src/components/cart.js
+++ src/components/cart.js
@@ -29,37 +29,63 @@ export function Cart() {
     );
   };
 
+  const getTotalCost = () => {
+    return itemsInCart.reduce(
+      (acc, item) => acc + item.quantity * item.price,
+      0
+    );
+  };
+
   return (
     <div className="container">
       <h1>Cart</h1>
-      <div className="cards-section">
-        {itemsInCart.map((item) => (
-          <div className="card">
-            <img className="card-img" src={item.img} alt="" />
 
-            <div className="card-info">
-              <p className="card-title">{item.name}</p>
-              <small className="card-price">Rs.{item.price}</small>
+      <div className="cart-page">
+        <div className="cards-section cart-list">
+          <h3>Items in Cart</h3>
+          {itemsInCart.map((item) => (
+            <div className="card">
+              <img className="card-img" src={item.img} alt="" />
 
-              <div className="cart-update-quantity">
-                <button
-                  className="button button-incdec"
-                  onClick={() => decrementQuantity(item.id)}
-                >
-                  -
-                </button>
-                <small className="cart-quantity">{item.quantity}</small>
+              <div className="card-info">
+                <p className="card-title">{item.name}</p>
+                <small className="card-price">Rs.{item.price}</small>
 
-                <button
-                  className="button button-incdec"
-                  onClick={() => incrementQuantity(item.id)}
-                >
-                  +
-                </button>
+                <div className="cart-update-quantity">
+                  <button
+                    className="button button-incdec"
+                    onClick={() => decrementQuantity(item.id)}
+                  >
+                    -
+                  </button>
+                  <small className="cart-quantity">{item.quantity}</small>
+
+                  <button
+                    className="button button-incdec"
+                    onClick={() => incrementQuantity(item.id)}
+                  >
+                    +
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <div className="cart-details">
+          <h3>
+            Total Cost :{" "}
+            <span
+              style={{
+                backgroundColor: "#ECECEC",
+                padding: "0.5rem",
+              }}
+            >
+              {" "}
+              Rs. {itemsInCart.length ? getTotalCost() : 0}{" "}
+            </span>
+          </h3>{" "}
+          <button className="button">Proceed to checkout</button>
+        </div>
       </div>
     </div>
   );
