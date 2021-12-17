f078a711a317144f8368b7ff2b0f2f81acba8bc4
MadhushreeKunder <madhushreekunder@gmail.com>
2021-12-18 01:05:07 +0530

    add wishlist component

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,5 @@ export { products } from "./data";
 export { ProductListing } from "./productListing";
 export { Cart } from "./cart";
 export { CartHeader } from "./cartHeader";
+export { WishList } from "./wishList";
 
