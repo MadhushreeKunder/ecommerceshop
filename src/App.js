27d3beb53a2b8bc673619ae350adc7a3f128139f
MadhushreeKunder <madhushreekunder@gmail.com>
2021-12-31 19:34:42 +0530

    change header

--- src/App.js
+++ src/App.js
@@ -6,7 +6,55 @@ export default function App() {
   const [route, setRoute] = useState("products");
   return (
     <div className="App">
-      <h1 className="app-header"> Coral-Shop</h1>
+      <header className="nav-header">
+        <div className="navigation-desktop">
+          <a className="nav-logo flex" href="#">
+            <img src="images/logo1.png" className="logo" alt="CORAL-UI"></img>
+            <h1>CORAL</h1>
+          </a>
+          <nav className="nav">
+            <ul className="nav-list nav-list--primary">
+              <li className="nav-item">
+                <button
+                  className="nav-link button button-primary"
+                  onClick={() => setRoute("products")}
+                >
+                  Home
+                </button>
+              </li>
+              <li className="nav-item">
+                <button
+                  className="nav-link button button-primary"
+                  onClick={() => setRoute("cart")}
+                >
+                  Cart
+                </button>
+              </li>
+              <li className="nav-item">
+                <button
+                  className="nav-link button button-primary"
+                  onClick={() => setRoute("wishlist")}
+                >
+                  Wishlist
+                </button>
+              </li>
+            </ul>
+            <ul className="nav-list nav-list--secondary">
+              <li className="nav-item">
+                <button className="nav-link button button-primary">
+                  Sign In
+                </button>
+              </li>
+              <li className="nav-item">
+                <button className="nav-link button button-primary">
+                  Sign up
+                </button>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+
       <div className="app-body">
         <CartHeader />
         <button
@@ -14,24 +62,22 @@ export default function App() {
           onClick={() => setRoute("products")}
         >
           Products
-        </button>
+        </button>{" "}
         <button
           className={route === "cart" ? "button-primary button" : "button"}
           onClick={() => setRoute("cart")}
         >
           Cart
-        </button>
-
+        </button>{" "}
         <button
           className={route === "wishlist" ? "button-primary button" : "button"}
           onClick={() => setRoute("wishlist")}
         >
           Wishlist
         </button>
-
         {route === "products" && <ProductListing />}
         {route === "cart" && <Cart />}
-        {route === "wishlist" && <WishList/>}
+        {route === "wishlist" && <WishList />}
       </div>
     </div>
   );
