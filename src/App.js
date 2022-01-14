d0b06e1f7405e4fd3cd44f775ba78d31ac6f9a30
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-14 14:22:20 +0530

    change top nav

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
 import "./styles.css";
-import { ProductListing, Cart, CartHeader, WishList, Home, ProductDetail } from "./components";
+import {
+  ProductListing,
+  Cart,
+  CartHeader,
+  WishList,
+  Home,
+  ProductDetail,
+} from "./components";
 import { Link, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <div className="App">
       <header className="nav-header">
-        <div className="navigation-desktop">
-          <a className="nav-logo flex" href="google.com">
-            <img src="images/logo1.png" className="logo" alt="CORAL-UI"></img>
+        <div class="nav-top nav-top-flex">
+          <button class="nav-toggle" aria-label="open navigation">
+            <span class="hamburger"></span>
+          </button>
+          <a className="nav-logo flex" href="#">
+            <img
+              src="images/logo1.png"
+              className="logo-img"
+              alt="CORAL-UI"
+            ></img>
             <h1>CORAL</h1>
           </a>
-          <nav className="nav">
-            <ul className="nav-list nav-list--primary">
-              <li className="nav-item">
+          <nav class="nav">
+            <ul class="nav-list nav-list--primary">
+              <li class="nav-item">
                 <Link className="nav-link button button-primary" to="/">
                   Home{" "}
                 </Link>
               </li>
-              <li className="nav-item">
+              <li class="nav-item">
                 <Link className="nav-link button button-primary" to="/products">
                   Products
                 </Link>
               </li>
-              <li className="nav-item">
+              <li class="nav-item">
                 <Link className="nav-link button button-primary" to="/cart">
                   Cart
                 </Link>
@@ -32,6 +46,18 @@ export default function App() {
                 <Link className="nav-link button button-primary" to="/wishlist">
                   WishList
                 </Link>
+              </li>
+            </ul>
+            <ul class="nav-list nav-list--secondary">
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  Log In
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link nav-link--button">
+                  Sign up
+                </a>
               </li>
             </ul>
           </nav>
