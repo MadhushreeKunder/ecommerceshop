167505d4e91538faeb5155eba6aff0914495154b
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-14 14:21:56 +0530

    add product detail, try add cart wishlist buttons

--- src/components/productDetail.js
+++ src/components/productDetail.js
@@ -1,20 +1,75 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { products } from "../data";
+import { useCart, useWishList } from "../contexts";
 
 export function ProductDetail() {
   const { productId } = useParams();
 
   console.log({ productId });
-  const {name, description, img, price} = products.find((product) => product.id === Number(productId));
+  const {
+    item,
+    name,
+    brand,
+    description,
+    img,
+    price,
+    oldprice,
+    discount,
+    rating,
+    star,
+    id,
+    quantity,
+  } = products.find((product) => product.id === Number(productId));
+
+  const { itemsInCart, setItemsInCart } = useCart();
+  const { itemsInWishList, setItemsInWishList } = useWishList();
+
+  //   const addToCart = (product) => {
+  //     const itemExists = itemsInCart.find((item) => product.name === item.name);
+  //     if (!itemExists) {
+  //       setItemsInCart([...itemsInCart, { ...product, quantity: 1 }]);
+  //     }
+  //   };
+
+  //   const addToWishList = (product) => {
+  //     const itemExists = itemsInWishList.find(
+  //       (item) => product.name === item.name
+  //     );
+  //     if (!itemExists) {
+  //       setItemsInWishList([...itemsInWishList, product]);
+  //       // setToggleHeart("red");
+  //     }
+  //   };
 
   return (
-    <div>
-        <img src={img}/>
-      <h1> {name} </h1>
-      <h2>Price: Rs. {price}</h2>
-      <p>{description}</p>
-      I am a product
+    <div className="container product-detail-flex">
+      <div className=" pd product-detail-img">
+        <img src={img} />
+      </div>
+      <div className="pd product-detail-info">
+        <h1> {brand} </h1>
+        <h2 className="product-detail-name">{name}</h2>
+        <h2> Rs. {price}</h2>
+        <span className="product-detail-old-price"> Rs. {oldprice}</span>
+        <span className="product-detail-discount"> ({discount}% OFF)</span>
+        <p className="product-description">{description}</p>
+        <div className="flex">
+          <button
+            //   onClick={() => addToCart(item)}
+            className="button button-primary"
+          >
+            Add to Cart
+          </button>
+          {"  "}
+          <button
+            // onClick={() => addToWishList(item)}
+            className="button button-secondary"
+          >
+            Move to WishList
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
