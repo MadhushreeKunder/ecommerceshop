498002fd5f8a20728c00d9ddce24013324cb5ab2
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-21 20:25:55 +0530

    add homepage image and heading

--- src/components/home.js
+++ src/components/home.js
@@ -1,3 +1,21 @@
 export function Home() {
-  return <h1>I am home</h1>;
+  return (
+    <div>
+      <div
+        className="flex "
+        style={{
+          alignItems: "center",
+          justifyContent: "space-evenly",
+          margin: "4rem 2rem",
+          flexWrap: "wrap-reverse", 
+          wordWrap: "break-word"
+        }}
+      >
+        <h1 style={{ fontSize: "2.5rem" }}>
+          Shop your best looks on CORAL-store
+        </h1>
+        <img style={{ width: "100vw" }} src="/images/sapiens (3).png" alt="" />
+      </div>
+    </div>
+  );
 }
