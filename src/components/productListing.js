6401bb52b26747aa21b931dff921aa037667ec77
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-22 14:35:46 +0530

    remove Link from product detail

--- src/components/productListing.js
+++ src/components/productListing.js
@@ -36,44 +36,44 @@ export function ProductListing() {
     (async function () {
       setLoader(true);
       try {
-        const response = await axios.get("https://product-id-handler-middleware-timestamps-1.madhushreekunde.repl.co/products")
-        .then((response) => setProductsData(response.data.products));
+        const response = await axios
+          .get(
+            "https://product-id-handler-middleware-timestamps-1.madhushreekunde.repl.co/products"
+          )
+          .then((response) => setProductsData(response.data.products));
       } catch (error) {
         console.error(error);
       } finally {
-        setLoader( false);
+        setLoader(false);
       }
     })();
-  }, [])
-
+  }, []);
 
   const filterReducer = (state, action) => {
-    switch(action.type) {
+    switch (action.type) {
       case "SORT":
-        return (state = {...state, sortBy: action.payload});
+        return (state = { ...state, sortBy: action.payload });
 
-        default: 
+      default:
         return state;
     }
-  }
+  };
 
-  const [ {sortBy} , dispatch ] = useReducer( filterReducer, {sortBy:null} );
+  const [{ sortBy }, dispatch] = useReducer(filterReducer, { sortBy: null });
 
-  const getSortedData = ( productList, sortBy) => {
-    if ( sortBy && sortBy === "PRICE_HIGH_TO_LOW") {
-      return productList.sort((a,b) => b["price"] - a["price"]);
+  const getSortedData = (productList, sortBy) => {
+    if (sortBy && sortBy === "PRICE_HIGH_TO_LOW") {
+      return productList.sort((a, b) => b["price"] - a["price"]);
     }
 
-    if ( sortBy && sortBy === "PRICE_LOW_TO_HIGH") {
-      return productList.sort((a,b) => a["price"] - b["price"]);
+    if (sortBy && sortBy === "PRICE_LOW_TO_HIGH") {
+      return productList.sort((a, b) => a["price"] - b["price"]);
     }
 
     return productList;
-  }
-
+  };
 
   const sortedData = getSortedData(productsData, sortBy);
-
 
   return (
     <div className="container">
@@ -84,24 +84,26 @@ export function ProductListing() {
       <fieldset>
         <legend>Sort By</legend>
         <label>
-          <input 
-          type = "radio"
-          name = "sort"
-          onChange={() => dispatch({ type: "SORT", payload: "PRICE_HIGH_TO_LOW"})
-        }
-        checked= {sortBy && sortBy === "PRICE_HIGH_TO_LOW"}
-        ></input>
-        Price - High to low {" "}
+          <input
+            type="radio"
+            name="sort"
+            onChange={() =>
+              dispatch({ type: "SORT", payload: "PRICE_HIGH_TO_LOW" })
+            }
+            checked={sortBy && sortBy === "PRICE_HIGH_TO_LOW"}
+          ></input>
+          Price - High to low{" "}
         </label>
         <label>
           <input
             type="radio"
             name="sort"
-            onChange={() => dispatch({ type: "SORT", payload: "PRICE_LOW_TO_HIGH" })
+            onChange={() =>
+              dispatch({ type: "SORT", payload: "PRICE_LOW_TO_HIGH" })
             }
             checked={sortBy && sortBy === "PRICE_LOW_TO_HIGH"}
-          ></input>
-     {" "}     Price- Low to High
+          ></input>{" "}
+          Price- Low to High
         </label>
       </fieldset>
 
@@ -109,14 +111,17 @@ export function ProductListing() {
         {productsData.map((item) => (
           <div className="card">
             {/* <Link to={`/products/${item.id}`}> */}
-              <div className="card-img-rating">
-                <img className="card-img" src={item.img} alt="" />{" "}
-                <div className="bottom-left">
-                  {item.star} <span class="fa fa-star checked"> </span> |{" "}
-                  {item.rating}k
-                </div>
+
+            <div className="card-img-rating">
+              <img className="card-img" src={item.img} alt="" />{" "}
+              {/* <small class="card-overlay" style={{padding:0}}>Out of Stock</small> */}
+              <div className="bottom-left">
+                {item.star} <span class="fa fa-star checked"> </span> |{" "}
+                {item.rating}k
               </div>
+            </div>
             {/* </Link> */}
+
             <button
               class="button card-badge-small"
               onClick={() => addToWishList(item)}
@@ -128,14 +133,14 @@ export function ProductListing() {
             </button>
             <div className="card-info">
               {/* <Link to={`/products/${item.id}`}> */}
-                <div>
-                  <p className="card-brand">{item.brand}</p>
-                  <p className="card-name">{item.name}</p>
-                  {/* <p>{item.star} <span class="fa fa-star checked"> </span> | {item.rating}k Ratings</p> */}
-                  <p className="card-price">Rs.{item.price} </p>
-                  <span className="card-old-price"> Rs. {item.oldprice}</span>
-                  <span className="card-discount"> ({item.discount}% OFF)</span>
-                </div>
+              <div>
+                <p className="card-brand">{item.brand}</p>
+                <p className="card-name">{item.name}</p>
+                {/* <p>{item.star} <span class="fa fa-star checked"> </span> | {item.rating}k Ratings</p> */}
+                <p className="card-price">Rs.{item.price} </p>
+                <span className="card-old-price"> Rs. {item.oldprice}</span>
+                <span className="card-discount"> ({item.discount}% OFF)</span>
+              </div>
               {/* </Link> */}
 
               <button
