6fdbd065c6834e698660a02f8e08051c2a687a52
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-22 15:08:56 +0530

    feat: add inventory and fast delivery filter

--- src/components/productListing.js
+++ src/components/productListing.js
@@ -53,27 +53,37 @@ export function ProductListing() {
     switch (action.type) {
       case "SORT":
         return (state = { ...state, sortBy: action.payload });
+      case "TOGGLE_INVENTORY":
+        return (state = { ...state, showInventoryAll : !state.showInventoryAll});
+      case "TOGGLE_DELIVERY":
+        return (state = {...state, showFastDeliveryOnly : !state.showFastDeliveryOnly})
 
       default:
         return state;
     }
   };
 
-  const [{ sortBy }, dispatch] = useReducer(filterReducer, { sortBy: null });
+  const [{ sortBy, showInventoryAll, showFastDeliveryOnly }, dispatch] = useReducer(filterReducer, { sortBy: null, showInventoryAll: true, showFastDeliveryOnly: false });
 
   const getSortedData = (productList, sortBy) => {
     if (sortBy && sortBy === "PRICE_HIGH_TO_LOW") {
       return productList.sort((a, b) => b["price"] - a["price"]);
     }
-
     if (sortBy && sortBy === "PRICE_LOW_TO_HIGH") {
       return productList.sort((a, b) => a["price"] - b["price"]);
     }
-
     return productList;
   };
 
+  const getFilteredData = (productList, {showInventoryAll, showFastDeliveryOnly}) => {
+    return productList
+    .filter(({ inStock }) => (showInventoryAll ? true : inStock))
+    .filter(({ fastDelivery }) => (showFastDeliveryOnly ? fastDelivery : true))
+  }
+
+
   const sortedData = getSortedData(productsData, sortBy);
+  const filteredData = getFilteredData(sortedData, {showInventoryAll, showFastDeliveryOnly})
 
   return (
     <div className="container">
@@ -107,14 +117,35 @@ export function ProductListing() {
         </label>
       </fieldset>
 
+      <fieldset style={{ marginTop: "1rem" }}>
+        <legend> Filters </legend>
+        <label>
+          <input
+            type="checkbox"
+            checked={showInventoryAll}
+            onChange={() => dispatch({ type: "TOGGLE_INVENTORY" })}
+          />
+         {" "} Include Out of Stock
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            checked={showFastDeliveryOnly}
+            onChange={() => dispatch({ type: "TOGGLE_DELIVERY" })}
+          />
+          Fast Delivery Only
+        </label>
+        </fieldset>
+
       <div className="cards-section">
-        {productsData.map((item) => (
+        {filteredData.map((item) => (
           <div className="card">
             {/* <Link to={`/products/${item.id}`}> */}
 
             <div className="card-img-rating">
-              <img className="card-img" src={item.img} alt="" />{" "}
-              {/* <small class="card-overlay" style={{padding:0}}>Out of Stock</small> */}
+              <img className={item.inStock? "card-img" : "card-img card-filter"} src={item.img} alt="" />{" "}
+              {!item.inStock && <small class="card-overlay" style={{padding:0}}>Out of Stock</small> }
+             
               <div className="bottom-left">
                 {item.star} <span class="fa fa-star checked"> </span> |{" "}
                 {item.rating}k
@@ -144,7 +175,7 @@ export function ProductListing() {
               {/* </Link> */}
 
               <button
-                className="button button-primary card-button"
+                className= {item.inStock? "button button-primary card-button" : "button card-button button-disable"}
                 onClick={() => addToCart(item)}
               >
                 Add to Cart
