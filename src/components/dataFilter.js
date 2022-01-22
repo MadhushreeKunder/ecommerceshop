38b25332bb42b07c18cc78a515b84f683804c327
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-22 17:13:01 +0530

    refactor: datafilter in components

--- src/components/dataFilter.js
+++ src/components/dataFilter.js
@@ -1,22 +1,18 @@
-
-
- export const getSortedData = (productList, sortBy) => {
-    if (sortBy && sortBy === "PRICE_HIGH_TO_LOW") {
-      return productList.sort((a, b) => b["price"] - a["price"]);
-    }
-    if (sortBy && sortBy === "PRICE_LOW_TO_HIGH") {
-      return productList.sort((a, b) => a["price"] - b["price"]);
-    }
-    return productList;
-  };
+export const getSortedData = (productList, sortBy) => {
+  if (sortBy && sortBy === "PRICE_HIGH_TO_LOW") {
+    return productList.sort((a, b) => b["price"] - a["price"]);
+  }
+  if (sortBy && sortBy === "PRICE_LOW_TO_HIGH") {
+    return productList.sort((a, b) => a["price"] - b["price"]);
+  }
+  return productList;
+};
 
 export const getFilteredData = (
-    productList,
-    { showInventoryAll, showFastDeliveryOnly }
-  ) => {
-    return productList
-      .filter(({ inStock }) => (showInventoryAll ? true : inStock))
-      .filter(({ fastDelivery }) =>
-        showFastDeliveryOnly ? fastDelivery : true
-      );
-  };
+  productList,
+  { showInventoryAll, showFastDeliveryOnly }
+) => {
+  return productList
+    .filter(({ inStock }) => (showInventoryAll ? inStock : true))
+    .filter(({ fastDelivery }) => (showFastDeliveryOnly ? fastDelivery : true));
+};
