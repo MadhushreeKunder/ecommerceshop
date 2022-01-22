ad8f1cdd7aed2c3286c59338fdc426ed72c1d48b
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-22 20:48:28 +0530

    feat: style home page & responsive

--- src/components/home.js
+++ src/components/home.js
@@ -1,21 +1,26 @@
+import { Link } from "react-router-dom";
+
 export function Home() {
   return (
     <div>
-      <div
-        className="flex "
-        style={{
-          alignItems: "center",
-          justifyContent: "space-evenly",
-          margin: "4rem 2rem",
-          flexWrap: "wrap-reverse", 
-          wordWrap: "break-word"
-        }}
-      >
-        <h1 style={{ fontSize: "2.5rem" }}>
-          Shop your best looks on CORAL-store
-        </h1>
-        <img style={{ width: "100vw" }} src="/images/sapiens (3).png" alt="" />
+      <div className="home-container">
+        <section className="hero-info">
+          <h1 className="hero-text">
+            Shop your best looks on <br />{" "}
+            <span style={{ color: "#fe656b" }}>CORAL-store</span>
+          </h1>
+          <Link className="nav-link button hero-button" to="/products">
+            Shop Now
+          </Link>
+        </section>
+
+        <img
+          className="hero-image"
+          src="/images/Saly-34.png"
+          alt="3D person facing right"
+        />
       </div>
+      <footer>Made with ❤ by Madhushree Kunder</footer>
     </div>
   );
 }
