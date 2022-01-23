0ee1744701c38fd963ee2421f31b74544e68ba8d
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-24 02:39:06 +0530

    add userContext

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,3 +1,4 @@
 export { useCart, CartProvider } from "./cartContext";
 export { useWishList, WishListProvider} from "./wishListContext";
 export { useProduct, ProductProvider} from "./productContext";
+export { useUser, UserProvider } from "./userContext";
---
+++ src/contexts/userContext.js
@@ -0,0 +1,60 @@
+import {
+  createContext,
+  useState,
+  useContext,
+  useEffect,
+  useReducer,
+} from "react";
+import React from "react";
+import axios from "axios";
+import { useAuth } from "../auth/authContext";
+import { useNavigate } from "react-router-dom";
+import { userReducer } from "../reducers/userReducer";
+import { backendURL } from "../utils/utils";
+
+export const UserContext = createContext();
+
+export function UserProvider({ children }) {
+  const { token } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get(
+          `${backendURL}/user-details`
+          // { headers: { authorization: token } }
+        );
+        const data = response.data.user;
+        dispatch({ type: "LOAD_USER_DETAILS", payload: data });
+      } catch (error) {
+        if (error.response.status === 401) {
+          //   navigate("/login");
+          console.error("error from usercontext");
+        }
+        console.log("UserContext error:", error.response);
+      }
+    })();
+  }, [
+    token,
+    // navigate
+  ]);
+
+  const [state, dispatch] = useReducer(userReducer, {
+    itemsInCart: [],
+    itemsInWishList: [],
+    _id: "1",
+    addresses: [],
+    loading: "",
+  });
+
+  return (
+    <UserContext.Provider value={{ itemsInCart: state.itemsInCart, dispatch }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export function useUser() {
+  return useContext(UserContext);
+}
