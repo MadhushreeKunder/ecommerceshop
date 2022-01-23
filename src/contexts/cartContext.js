6712a7c2d4c9b55053cbbe756985da3531d08409
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-23 23:56:01 +0530

    get data from backend in cart provider

--- src/contexts/cartContext.js
+++ src/contexts/cartContext.js
@@ -1,16 +1,56 @@
-import { createContext, useState, useContext } from "react";
+import {
+  createContext,
+  useState,
+  useContext,
+  useEffect,
+  useReducer,
+} from "react";
+import React from "react";
+import axios from "axios";
+import { useAuth } from "../auth/authContext";
+import { useNavigate } from "react-router-dom";
+import { cartReducer } from "../reducers/cartReducer";
+import { backendURL } from "../utils/utils";
 
 export const CartContext = createContext();
 
-export function CartProvider({children}) {
-    const [ itemsInCart, setItemsInCart ] = useState([]);
-    return (
-        <CartContext.Provider value = {{itemsInCart, setItemsInCart}}>
-            {children}
-        </CartContext.Provider>
-    );
+export function CartProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const { token } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get(
+         `${backendURL}/user`,
+          { headers: { authorization: token } }
+        );
+        setUser(response.data);
+      } catch (error) {
+        if (error.response.status === 401) {
+          navigate("/login");
+        }
+        setUser("error");
+      }
+    })();
+  }, [token, navigate]);
+
+  const [state, dispatch] = useReducer(cartReducer, {
+    itemsInCart: [],
+    // itemsInWishList: [],
+    _id: "1",
+    // addresses: [],
+    // loading: "",
+  });
+
+  return (
+    <CartContext.Provider value={{ itemsInCart: state.itemsInCart, dispatch }}>
+      {children}
+    </CartContext.Provider>
+  );
 }
 
 export function useCart() {
-    return useContext(CartContext);
+  return useContext(CartContext);
 }
