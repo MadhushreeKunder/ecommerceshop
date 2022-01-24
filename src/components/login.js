f7851c30cb45604a67c86804b2c492558f76c839
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-24 23:46:38 +0530

    login page done

--- src/components/login.js
+++ src/components/login.js
@@ -53,41 +53,76 @@ export function Login() {
   return (
     <div className="login-form">
       <h2>Login</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit consequatur
-        excepturi, dignissimos laborum expedita velit voluptatem. Amet
-        accusantium quidem eum aliquam odit perspiciatis voluptate explicabo
-        quos expedita nesciunt delectus, vitae impedit sit velit sed tempora
-        voluptas recusandae omnis quis non rem molestias iusto veniam! Corrupti
-        tempora voluptatum autem ex cum et debitis ipsum aperiam. Esse doloribus
-        repellat sapiente cumque temporibus, deleniti rem odio iste quod minus
-        animi vitae! Cupiditate non quam ex, voluptas, nesciunt placeat
-        architecto cumque quae, enim ipsum repudiandae iure id sequi nam at!
-        Maiores facilis, debitis sequi doloribus, minima vitae fuga nam dolores
-        hic unde nihil ducimus! Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Eaque voluptatum provident esse! Suscipit
-        necessitatibus tenetur minus. Ipsum aliquid voluptatum dignissimos
-        dolores, debitis minus quae. Laborum veniam voluptates ratione corporis
-        nesciunt debitis sapiente vitae nobis dignissimos nulla in unde
-        asperiores, sint eum maiores quis placeat! Sapiente, delectus velit
-        pariatur commodi explicabo provident eligendi dolore praesentium ipsum
-        assumenda cum possimus perspiciatis quaerat doloribus deserunt, vero, id
-        rem aut saepe adipisci ex repudiandae! Itaque, dicta temporibus id porro
-        eveniet, quas totam quos deleniti, esse eaque neque ex vel eos
-        consectetur expedita nostrum similique voluptatibus consequuntur numquam
-        suscipit odit! Qui fugiat voluptas odit facilis adipisci enim soluta
-        incidunt natus, dolorem quisquam ipsam eligendi placeat excepturi, aut
-        asperiores harum autem! Obcaecati doloremque aut dolorem sit! Inventore,
-        quas nulla, delectus ab ducimus repellendus, ipsam numquam praesentium
-        distinctio et saepe modi blanditiis tenetur? Perspiciatis dolore quam
-        eum laborum aliquam eveniet cum eius necessitatibus. Nam cupiditate
-        voluptatum totam officiis commodi accusantium expedita sequi aspernatur
-        nostrum assumenda accusamus explicabo inventore distinctio illum culpa
-        illo eius, eligendi rem ea pariatur earum? Quidem dolorum id
-        voluptatibus officia et magni neque vero veniam quae, quia natus
-        doloribus unde numquam eius illum exercitationem, laudantium fuga harum
-        cumque in enim. Excepturi odit repellat enim?
-      </p>
+      <div className="login-input">
+        <label>
+          <i className="fas fa-user"></i> Username
+        </label>
+        <input
+          type="text"
+          placeholder="Enter username"
+          className="input-txt-error form-input"
+          required
+          value={loginCredentials.username}
+          onChange={(e) =>
+            setLoginCredentials(() => ({
+              ...loginCredentials,
+              msg: "",
+              username: e.target.value,
+            }))
+          }
+        />
+      </div>
+
+      <div className="login-input ">
+        <label>
+          <i className="fas fa-lock"></i> Password
+        </label>
+        <input
+          type={loginCredentials.showPassword ? "text" : "password"}
+          placeholder="Enter password"
+          className="input-txt-error form-input"
+          required
+          value={loginCredentials.password}
+          onChange={(e) =>
+            setLoginCredentials(() => ({
+              ...loginCredentials,
+              msg: "",
+              password: e.target.value,
+            }))
+          }
+        />
+
+        <button
+          className="login-show-password"
+          onClick={() =>
+            setLoginCredentials(() => ({
+              ...loginCredentials,
+              showPassword: !loginCredentials.showPassword,
+            }))
+          }
+        >
+          {loginCredentials.showPassword ? (
+            <i className="far fa-lg fa-eye"></i>
+          ) : (
+            <i className="far fa-lg  fa-eye-slash"></i>
+          )}
+        </button>
+      </div>
+      <h3>
+        {status.loading && (
+          <img src="../images/loading.svg" alt="loading" className="loading" />
+        )}
+      </h3>
+      <p>{loginCredentials.msg}</p>
+      <button className="button button-primary login-button" onClick={loginUser}>
+        Login
+      </button>
+      <small>
+        Don't have an account?{" "}
+        <Link to="/signup">
+          <span className="login-signup"> Sign up!</span>
+        </Link>
+      </small>
     </div>
   );
 }
