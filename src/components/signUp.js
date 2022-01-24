f23570d0dc1c56df2494064da17a3092b0fcdfd6
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-24 22:42:32 +0530

    sign up page initial

---
+++ src/components/signUp.js
@@ -0,0 +1,20 @@
+export function SignUp() {
+  return (
+    <>
+      <h2> Sign up</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+        optio debitis deleniti quisquam autem! Corporis perspiciatis incidunt,
+        velit, quo nihil vitae dolores quod sequi iure, commodi eius ducimus non
+        id natus a illum? Odit omnis pariatur dolorem sed deleniti id recusandae
+        facere tempora, quo est in iure esse quia eius numquam repellendus
+        nostrum explicabo autem eos. Laboriosam, unde officia pariatur iure
+        minima repudiandae non nam, quibusdam culpa ea totam tempora, dolor
+        dicta. Quis molestias dolorem, deserunt dignissimos sapiente repudiandae
+        porro sequi et. Doloribus, corporis, rem quos ipsa consequuntur
+        voluptatum animi eligendi reiciendis, minima odio quas perspiciatis
+        saepe dolor? Vel, eveniet.
+      </p>
+    </>
+  );
+}
