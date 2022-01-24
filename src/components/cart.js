bd3bbfd4092d881e185d79d38cd951b79ec3865f
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-24 18:43:43 +0530

    refactor cart

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,4 +1,9 @@
-import { useCart } from "../contexts";
+import { useUser } from "../contexts";
+import {
+  removeFromCartApi,
+  incrementQuantityCartApi,
+  decrementQuantityCartApi,
+} from "../api/apiSync";
 
 export function Cart() {
   // const { itemsInCart, setItemsInCart } = useCart();
@@ -29,10 +34,10 @@ export function Cart() {
   //   );
   // };
 
-  const { state, dispatch } = useUser();
+  const { state, dispatch: cartDispatch } = useUser();
 
-  const getTotalCost = () => {
-    return itemsInCart.reduce(
+  const getTotalCost = (item) => {
+    return state?.itemsInCart.reduce(
       (acc, item) => acc + item.quantity * item.price,
       0
     );
@@ -43,51 +48,62 @@ export function Cart() {
       <h1>Cart</h1>
 
       <div className="cart-page">
-        <div className="cards-section cart-list">
-          <h3>Items in Cart</h3>
-          {itemsInCart.map((item) => (
-            <div className="card">
-              <img className="card-img" src={item.img} alt="" />
+        {state?.itemsInCart.length === 0 ? (
+          <h1>Your cart feels light, add some Products!</h1>
+        ) : (
+          <div>
+            <div className="cards-section cart-list">
+              <h3>Items in Cart</h3>
+              {state?.itemsInCart.map((item) => (
+                <div className="card">
+                  <img className="card-img" src={item.img} alt="" />
 
-              <div className="card-info">
-                <p className="card-title">{item.name}</p>
-                <small className="card-price">Rs.{item.price}</small>
+                  <div className="card-info">
+                    <p className="card-title">{item.name}</p>
+                    <small className="card-price">Rs.{item.price}</small>
 
-                <div className="cart-update-quantity">
-                  <button
-                    className="button button-incdec"
-                    onClick={() => decrementQuantity(item.id)}
-                  >
-                    -
-                  </button>
-                  <small className="cart-quantity">{item.quantity}</small>
+                    <div className="cart-update-quantity">
+                      <button
+                        className="button button-incdec"
+                        onClick={() =>
+                          decrementQuantityCartApi(item, cartDispatch)
+                        }
+                      >
+                        -
+                      </button>
+                      <small className="cart-quantity">{item.quantity}</small>
 
-                  <button
-                    className="button button-incdec"
-                    onClick={() => incrementQuantity(item.id)}
-                  >
-                    +
-                  </button>
+                      <button
+                        className="button button-incdec"
+                        onClick={() =>
+                          incrementQuantityCartApi(item, cartDispatch)
+                        }
+                      >
+                        +
+                      </button>
+                    </div>
+                  </div>
                 </div>
-              </div>
+              ))}
             </div>
-          ))}
-        </div>
-        <div className="cart-details">
-          <h3>
-            Total Cost :{" "}
-            <span
-              style={{
-                backgroundColor: "#ECECEC",
-                padding: "0.5rem",
-              }}
-            >
-              {" "}
-              Rs. {itemsInCart.length ? getTotalCost() : 0}{" "}
-            </span>
-          </h3>{" "}
-          <button className="button">Proceed to checkout</button>
-        </div>
+            <div className="cart-details">
+              <h3>
+                Total Cost :{" "}
+                <span
+                  style={{
+                    backgroundColor: "#ECECEC",
+                    padding: "0.5rem",
+                  }}
+                >
+                  {" "}
+                   Rs.{" "}
+                  {state?.itemsInCart.length ? getTotalCost() : 0}{" "}
+                </span>
+              </h3>{" "}
+              <button className="button">Proceed to checkout</button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
