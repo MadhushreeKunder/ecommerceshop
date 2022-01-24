d0f6dcbbe8ed5b38cb56a9abad79f28863bdc0df
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-24 16:21:11 +0530

    fix: change backend api back to repl

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,2 +1,3 @@
-export const backendURL =
-  "https://product-id-handler-middleware-timestamps-1.madhushreekunde.repl.co";
+export const backendURL = "https://coral-store-api-backend.madhushreekunde.repl.co";
+// "https://coral-store-api.herokuapp.com/";
+// "https://product-id-handler-middleware-timestamps-1.madhushreekunde.repl.co";
