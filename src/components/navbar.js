c2613d6edc4c27018f2eb600b7bcca01a78fca61
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-25 02:17:25 +0530

    refactor

---
+++ src/components/navbar.js
@@ -0,0 +1,87 @@
+import { NavLink, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { useAuth } from "../auth/authContext";
+import { useUser } from "../contexts";
+
+export function NavBar() {
+  const { token } = useAuth();
+  const { state } = useUser();
+  const [isNavVisible, setNavVisible] = useState("false");
+
+  const toggleNav = () => {
+    setNavVisible(!isNavVisible);
+  };
+  const totalItems = () => {
+    return state?.itemsInCart.reduce((acc, value) => {
+      return acc + value.quantity;
+    }, 0);
+  };
+
+  return (
+    <>
+      <header className="nav-header">
+        <div className="nav-top nav-top-flex">
+          <button
+            className="nav-toggle"
+            aria-label="open navigation"
+            onClick={toggleNav}
+          >
+            <span className="hamburger"></span>
+          </button>
+
+          <NavLink end to="/">
+            <img
+              src="images/logo1.png"
+              className="logo-img"
+              alt="CORAL-UI"
+            ></img>
+          </NavLink>
+          <nav className={isNavVisible ? "nav" : "nav nav--visible"}>
+            <ul className="nav-list nav-list--primary">
+              <li className="nav-item">
+                <NavLink className="nav-link button" to="/products">
+                  Products
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link button"
+                  data-badge={token && totalItems()}
+                //   {itemsInCart.length}
+                  to="/cart"
+                >
+                  Cart
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link button"
+                  data-badge={token && state?.wishList.length}
+                  // {itemsInWishList.length}
+                  to="/wishlist"
+                > 
+                  WishList
+                </NavLink>
+              </li>
+            </ul>
+            <ul className="nav-list nav-list--secondary">
+              <li className="nav-item">
+                <NavLink className="nav-link button " to="/login">
+                  Log In{" "}
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link button button-secondary nav-link--button"
+                  to="/signup"
+                >
+                  Sign Up{" "}
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
+}
