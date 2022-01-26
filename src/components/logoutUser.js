8eaa9bdcdcecf91b1f0366849b09825452604b02
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-26 19:49:23 +0530

    add logout page

--- src/components/logoutUser.js
+++ src/components/logoutUser.js
@@ -1,12 +1,26 @@
+import { Link } from "react-router-dom";
 import { useAuth } from "../auth/authContext";
 
 export const LogoutUser = () => {
   const { user, logout } = useAuth();
   return (
-      <div>
-          <h2> Hello {user.username}</h2>
-          <p>{user.email}</p>
-          <button onClick={()=> logout()}>Logout</button>
-      </div>
-  )
+    <div style={{paddingBottom : "0"}} className="login-form">
+      <h2> Hello, {user.username}</h2>
+      <p>Shop more!</p>
+      <button  style={{ margin: "1.5rem" }} className="button button-primary">
+        {" "}
+        <Link to="/products">Shop More</Link>
+      </button>
+      <p>Do you want to logout?</p>
+      <button
+        style={{ margin: "1.5rem" }}
+        className="button"
+        onClick={() => logout()}
+      >
+        Logout
+      </button>
+      <img style={{width: "inherit"}} src="/images/Saly-17.png" alt="" />
+      
+    </div>
+  );
 };
