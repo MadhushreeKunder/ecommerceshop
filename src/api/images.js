6c2a0e954c615228a6a7b14bbcfe11e2ce24ef0c
MadhushreeKunder <madhushreekunder@gmail.com>
2022-01-27 00:35:37 +0530

    remove/add images

---
+++ src/api/images.js
@@ -0,0 +1,5 @@
+export const emptyCartImage = "https://res.cloudinary.com/madhushreekunder/image/upload/v1643223162/e-comm%20images/e-comm%203D%20images/Saly-1.png";
+export const emptyWishListImage = "https://res.cloudinary.com/madhushreekunder/image/upload/v1643223169/e-comm%20images/e-comm%203D%20images/Saly-36.png";
+export const homePageHeroImage = "https://res.cloudinary.com/madhushreekunder/image/upload/v1643223181/e-comm%20images/e-comm%203D%20images/Saly-34.png";
+export const logoutImage = "https://res.cloudinary.com/madhushreekunder/image/upload/v1643223175/e-comm%20images/e-comm%203D%20images/Saly-17.png";
+export const logoImage = "https://res.cloudinary.com/madhushreekunder/image/upload/v1643223204/e-comm%20images/e-comm%203D%20images/logo1.png";
