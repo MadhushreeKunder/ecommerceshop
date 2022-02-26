0b1ce312c2cdae831e95ef8fbc4da0c064841b6a
MadhushreeKunder <madhushreekunder@gmail.com>
2022-02-26 08:36:53 +0530

    fix dupe array

--- src/components/dataFilter.js
+++ src/components/dataFilter.js
@@ -1,9 +1,9 @@
 export const getSortedData = (productList, sortBy) => {
   if (sortBy && sortBy === "PRICE_HIGH_TO_LOW") {
-    return productList.sort((a, b) => b["price"] - a["price"]);
+    return [...productList].sort((a, b) => b["price"] - a["price"]);
   }
   if (sortBy && sortBy === "PRICE_LOW_TO_HIGH") {
-    return productList.sort((a, b) => a["price"] - b["price"]);
+    return [...productList].sort((a, b) => a["price"] - b["price"]);
   }
   return productList;
 };
@@ -11,7 +11,7 @@ export const getSortedData = (productList, sortBy) => {
 export const getFilteredData = (
   productList,
   { showInventoryAll, showFastDeliveryOnly }
-) => {
+) => {  
   return productList
     .filter(({ inStock }) => (showInventoryAll ? true : inStock))
     .filter(({ fastDelivery }) => (showFastDeliveryOnly ? fastDelivery : true));
