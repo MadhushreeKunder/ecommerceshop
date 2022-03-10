34c5291d0a58cf0b0957728dbcdbf4b3230a4274
MadhushreeKunder <madhushreekunder@gmail.com>
2022-03-10 15:50:22 +0530

    cart responsive

--- src/components/cart.js
+++ src/components/cart.js
@@ -21,96 +21,102 @@ export function Cart() {
 
       <div className="">
         {userState?.cart.length === 0 ? (
-          <EmptyCart/>
+          <EmptyCart />
         ) : (
-          <div className="cart">
-            <div className="cards-section cart-list">
-              <h3>Items in Cart</h3>
-              {userState?.cart.map((product) => (
-                <div className="card card-cart" key={product._id}>
-                  {/* <div className="card-cart-product"></div> */}
-                  <img
-                    className="card-cart-img"
-                    src={product.img}
-                    alt={product.name}
-                  />
+          <>
+            {/* <h3>Items in Cart</h3> */}
 
-                  <div className="card-cart-info ">
-                    <p className="card-brand">{product.brand}</p>
-                    <p className="card-name">{product.name}</p>
-                    <p className="card-price">Rs.{product.price}</p>
+            <div className="cart">
+              <div className="cards-section cart-list">
+                {userState?.cart.map((product) => (
+                  <div className="card card-cart" key={product._id}>
+                    {/* <div className="card-cart-product"></div> */}
+                    <img
+                      className="card-cart-img"
+                      src={product.img}
+                      alt={product.name}
+                    />
 
-                    <div className="cart-update-quantity">
+                    <div className="card-cart-info ">
+                      <p className="card-brand">{product.brand}</p>
+                      <p className="card-name">{product.name}</p>
+                      <p className="card-price">Rs.{product.price}</p>
+
+                      <div className="cart-update-quantity">
+                        <button
+                          className="button button-incdec button-secondary"
+                          onClick={() =>
+                            incrementQuantityCartApi(product, userDispatch)
+                          }
+                        >
+                          +
+                        </button>
+
+                        <small className="cart-quantity">
+                          {product.quantity}
+                        </small>
+
+                        {product.quantity > 1 ? (
+                          <button
+                            className="button button-incdec button-secondary"
+                            onClick={() =>
+                              decrementQuantityCartApi(product, userDispatch)
+                            }
+                          >
+                            -
+                          </button>
+                        ) : (
+                          <button
+                            className="button button-incdec button-secondary"
+                            onClick={() =>
+                              removeFromCartApi(product, userDispatch)
+                            }
+                          >
+                            -
+                          </button>
+                        )}
+                      </div>
+
                       <button
-                        className="button button-incdec button-secondary"
-                        onClick={() =>
-                          incrementQuantityCartApi(product, userDispatch)
-                        }
+                        className="button"
+                        style={{ marginBottom: "1rem" }}
+                        onClick={() => removeFromCartApi(product, userDispatch)}
                       >
-                        +
+                        Remove
                       </button>
-
-                      <small className="cart-quantity">
-                        {product.quantity}
-                      </small>
-
-                      {product.quantity > 1 ? (
-                        <button
-                          className="button button-incdec button-secondary"
-                          onClick={() =>
-                            decrementQuantityCartApi(product, userDispatch)
-                          }
-                        >
-                          -
-                        </button>
-                      ) : (
-                        <button
-                          className="button button-incdec button-secondary"
-                          onClick={() =>
-                            removeFromCartApi(product, userDispatch)
-                          }
-                        >
-                          -
-                        </button>
-                      )}
                     </div>
-
-                    <button
-                      className="button"
-                      onClick={() => removeFromCartApi(product, userDispatch)}
-                    >
-                      Remove
-                    </button>
+                    <div className="cart-card-total-price">
+                      Subtotal: Rs.{getTotalCostItem(product)}{" "}
+                    </div>
                   </div>
-                  <div>Price: Rs.{getTotalCostItem(product)} </div>
-                </div>
-              ))}
-            </div>
-            <div className="cart-payment">
-              <h3>
-                <span style={{ textDecoration: "underline" }}>SubTotal</span>
-                <p>
-                  ({totalItems(userState)} items) :{" "}
-                  <span
-                    style={{
-                      backgroundColor: "#ECECEC",
-                      padding: "0.5rem",
-                    }}
+                ))}
+              </div>
+              <div className="cart-payment">
+                <h3>
+                  <span style={{ textDecoration: "underline" }}>Total</span>
+                  <p>
+                    ({totalItems(userState)} items) :{" "}
+                    <span
+                      style={{
+                        backgroundColor: "#ECECEC",
+                        padding: "0.5rem",
+                      }}
+                    >
+                      Rs. {totalPrice(userState)}
+                    </span>
+                  </p>
+                </h3>{" "}
+                <Link to="/address">
+                  <button
+                    className="button button-primary"
+                    style={{ marginTop: "2rem" }}
                   >
-                    Rs. {totalPrice(userState)}
-                  </span>
-                </p>
-              </h3>{" "}
-              <Link to="/address">
-                <button
-                  className="button button-primary"
-                  style={{ marginTop: "2rem" }}
-                >
-                  Proceed to checkout
-                </button>
-              </Link>
+                    Proceed to checkout
+                  </button>
+                </Link>
+              </div>
             </div>
-          </div>
+          </>
         )}
       </div>
     </div>
